a4874c6999ea87c70c96049d26eed274bb0c4cc6
zhangpeng <406566640@qq.com>
2019-03-11 14:08:38 +0800

    撤销测试

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -89,8 +89,6 @@ def edit_entry(request, entry_id):
 	context = {'entry': entry, 'topic': topic, 'form':form}
 	return render(request, 'learning_logs/edit_entry.html', context)
 
-def test(request):
-	print('test')
 
 	
 	
